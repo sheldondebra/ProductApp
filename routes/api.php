78c8a846e3a0ec5335c71314c7b2bc176e510754
Sheldon Debra <54963942+sheldondebra@users.noreply.github.com>
2024-04-21 07:03:32 +0000

    Create Routes

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,24 @@
 <?php
 
-use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VendorController;
+use App\Http\Controllers\ProductController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/add-product',[ProductController::class, 'createproduct']);
-Route::get('/show-product',[ProductController::class, 'showproduct']);
+// Vendors Routes
+Route::get('/vendors', [VendorController::class, 'index']);
+Route::post('/vendors', [VendorController::class, 'store']);
+Route::get('/vendors/{id}', [VendorController::class, 'show']);
+Route::put('/vendors/{id}', [VendorController::class, 'update']);
+Route::delete('/vendors/{id}', [VendorController::class, 'destroy']);
+
+// Products Routes
+Route::get('/products', [ProductController::class, 'index']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::put('/products/{id}', [ProductController::class, 'update']);
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
